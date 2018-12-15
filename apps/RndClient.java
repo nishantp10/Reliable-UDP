c63dc4d57cf4bd3a27b05264b120018bcf828212
Sai Nishant Parepalli Laxman <38198912+nishantp10@users.noreply.github.com>
2018-12-15 13:33:50 -0500

    Update and rename RndClient.java to apps/RndClient.java

--- apps/RndClient.java
+++ apps/RndClient.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.nio.ByteBuffer;
 
-public class Client {
+public class RndClient {
 	
 	public static InetAddress try_ip() {
 	    	
