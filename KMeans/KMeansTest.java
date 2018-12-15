72f8b115f454a95371de58c20557c9b77f1f2ccb
Sai Nishant Parepalli Laxman <38198912+nishantp10@users.noreply.github.com>
2018-12-15 13:31:57 -0500

    Create KMeansTest.java

---
+++ KMeans/KMeansTest.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+
+public class KMeansTest {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		KMeans k = new KMeans();
+	
+		k.getCentroid();
+	}
+
+}
